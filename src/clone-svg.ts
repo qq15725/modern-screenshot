a9fad543f09b4fe21a3be5a358743c99423fb56c
Stein Strindhaug <stein.strindhaug@noaignite.com>
2023-08-28 15:40:05 +0200

    Test that href attribute starts with # before assuming it can be used as a querySelector string

--- src/clone-svg.ts
+++ src/clone-svg.ts
@@ -16,13 +16,16 @@ export async function cloneSvg<T extends SVGElement>(
       const id = use.getAttribute('xlink:href')
         ?? use.getAttribute('href')
 
-      if (!id) continue
+      if (id && id.startsWith('#')) { // the href attribute (probably) really is a query selector string
+        const definition = ownerDocument?.querySelector(`svg ${ id }`)
 
-      const definition = ownerDocument?.querySelector(`svg ${ id }`)
+        if (!definition || svgDefsElement?.querySelector(id)) continue
 
-      if (!definition || svgDefsElement?.querySelector(id)) continue
-
-      svgDefsElement?.appendChild(await cloneNode(definition, context))
+        svgDefsElement?.appendChild(await cloneNode(definition, context))
+      } else if (id) {
+        // TODO somehow handle xlink:href and href that is a URL with id-anchor
+        // ( see test/fixtures/svg.externalsymbol.html )
+      }
     }
   }
 
