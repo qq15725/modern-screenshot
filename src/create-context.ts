6f24f17c023cdecbb414fba1f56b890874190595
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-12-04 20:54:10 -0600

    feat: add `includeStyleProperties` option
    * the full list of computedStyle properties can be cached
    
    This commit is applied from https://github.com/bubkoo/html-to-image/pull/436

--- src/create-context.ts
+++ src/create-context.ts
@@ -57,6 +57,7 @@ export async function createContext<T extends Node>(node: T, options?: Options &
     onCloneNode: null,
     onEmbedNode: null,
     onCreateForeignObjectSvg: null,
+    includeStyleProperties: null,
     autoDestruct: false,
     ...options,
 
