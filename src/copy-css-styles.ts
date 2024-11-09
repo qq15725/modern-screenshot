afeb6f6d8c131d23a840c87a3371941b870f64da
wengxiangmin <157215725@qq.com>
2024-11-09 14:23:25 +0800

    fix: revert to before fix 4-bit precision

--- src/copy-css-styles.ts
+++ src/copy-css-styles.ts
@@ -3,8 +3,6 @@ import { getDefaultStyle } from './get-default-style'
 import { getDiffStyle } from './get-diff-style'
 import { IN_CHROME } from './utils'
 
-const px4PrecisionRE = /^\d+\.\d{4}px$/
-
 export function copyCssStyles<T extends HTMLElement | SVGElement>(
   node: T,
   cloned: T,
@@ -59,10 +57,6 @@ export function copyCssStyles<T extends HTMLElement | SVGElement>(
   }
 
   style.forEach(([value, priority], name) => {
-    // fix a typographic anomaly caused by getComputedStyle getting px values only to 4-bit precision
-    if (px4PrecisionRE.test(value)) {
-      value = `${Number.parseFloat(value) + 0.0001}px`
-    }
     clonedStyle.setProperty(name, value, priority)
   })
 
