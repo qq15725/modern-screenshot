e8b2d3f757874a052aac1961347662a10e007bbc
wxm <157215725@qq.com>
2025-02-13 10:36:55 +0800

    fix: log prefix (closes #129)

--- src/create-logger.ts
+++ src/create-logger.ts
@@ -6,12 +6,15 @@ export interface Logger {
   warn: (...args: any[]) => void
 }
 
+let uid = 0
 export function createLogger(debug: boolean): Logger {
+  const prefix = `${PREFIX}[#${uid}]`
+  uid++
   return {
     // eslint-disable-next-line no-console
-    time: (label: string) => debug && console.time(`${PREFIX} ${label}`),
+    time: (label: string) => debug && console.time(`${prefix} ${label}`),
     // eslint-disable-next-line no-console
-    timeEnd: (label: string) => debug && console.timeEnd(`${PREFIX} ${label}`),
+    timeEnd: (label: string) => debug && console.timeEnd(`${prefix} ${label}`),
     warn: (...args: any[]) => debug && consoleWarn(...args),
   }
 }
