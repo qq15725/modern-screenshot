ea2ddf43c81bce5982c6479524efd3f7752f5bf1
wxm <157215725@qq.com>
2025-06-12 11:12:39 +0800

    fix: use `HTMLIFrameElement.srcdoc` replace `HTMLIFrameElement.contentWindow.document.write` remove browser warning (closes #136)

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -8,13 +8,13 @@ export function getSandBox(context: Context): HTMLIFrameElement | undefined {
     try {
       if (ownerDocument) {
         sandbox = ownerDocument.createElement('iframe')
-        sandbox.id = `__SANDBOX__-${uuid()}`
+        sandbox.id = `__SANDBOX__${uuid()}`
         sandbox.width = '0'
         sandbox.height = '0'
         sandbox.style.visibility = 'hidden'
         sandbox.style.position = 'fixed'
         ownerDocument.body.appendChild(sandbox)
-        sandbox.contentWindow?.document.write('<!DOCTYPE html><meta charset="UTF-8"><title></title><body>')
+        sandbox.srcdoc = '<!DOCTYPE html><meta charset="UTF-8"><title></title><body>'
         context.sandbox = sandbox
       }
     }
