d2cdbb7547fe017eecf98cb62d786c7ddc2ec4f6
Tima Gixe <timagixe@protonmail.com>
2026-05-07 18:56:57 +0300

    fix: preserve selected option state when cloning inputs

--- src/copy-input-value.ts
+++ src/copy-input-value.ts
@@ -1,9 +1,24 @@
-import { isInputElement, isSelectElement, isTextareaElement } from './utils'
+import {
+  isInputElement,
+  isOptionElement,
+  isSelectElement,
+  isTextareaElement,
+} from './utils'
 
 export function copyInputValue<T extends HTMLElement | SVGElement>(
   node: T,
   cloned: T,
 ): void {
+  if (isOptionElement(node) && isOptionElement(cloned)) {
+    cloned.selected = node.selected
+
+    if (node.selected)
+      cloned.setAttribute('selected', '')
+    else cloned.removeAttribute('selected')
+
+    return
+  }
+
   if (isTextareaElement(node) || isInputElement(node) || isSelectElement(node)) {
     cloned.setAttribute('value', node.value)
   }
